21ef3246c935fd45d78dd79a490cf0d1d8e03a96
nidhikrishnan <nidhikrishnan@172.27.17.3>
2023-01-26 12:31:36 -0600

    Love ted drewes

---
+++ src/studio1/Average.java
@@ -0,0 +1,20 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the average of two integers");
+		int n1 = in.nextInt();
+		int n2 = in.nextInt();
+		double n3 = ((double)(n1 + n2))/2;
+		System.out.println((n3));
+		
+				
+		
+	}
+
+}
---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What year is it?");
+		int year = in.nextInt();
+		boolean isLeapYear = ((year%4==0)&&(year%100!=0)||(year%400==0));
+		System.out.println(isLeapYear);
+
+	}
+
+}
